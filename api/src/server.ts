c882c7702f4c4342c73a54c2afab242ecd02d742
Caio Souza <kcaiosouza@gmail.com>
2024-03-26 17:27:05 -0300

    Base da Aplicação - Cumida Arretada v0.1-alpha

---
+++ api/src/server.ts
@@ -0,0 +1,34 @@
+import fastify from "fastify";
+import { z } from "zod"; // Typa e valida variáveis
+
+const app = fastify();
+
+// POST
+app.post('/api/v0.1/simpleGetInfoFromBody', (request) => {
+  const {string1, number1, url1} = z.object({
+		string1: z.string(),
+		number1: z.number(),
+		url1: z.string().url(),
+	}).parse(request.body)
+
+  return {
+    String: string1,
+    Number: number1,
+		Url: url1
+  }
+})
+
+// GET
+app.get('/api/v0.1/simpleReturn', () => {
+  return {
+    Retorno: "Simples texto de retorno!"
+  }
+})
+
+// UPDATE (app.update), DELETE (app.delete) e PUT (app.put)
+
+app.listen({
+  port: 3333,
+}).then(() => {
+  console.log("HTTP server running!")
+})
