14a969e6951f374ff6833e4e53374c41c3ca0fae
Caio Souza <kcaiosouza@gmail.com>
2024-04-06 17:33:38 -0300

    card review

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -2,7 +2,17 @@ import { GetStaticProps } from "next";
 
 export default function Home() {
   return (
-    <h1>Pagina Principal</h1>
+    <main>
+      <header className="bg-gradient-to-br from-white from-25% to-[#FF7F63] h-screen w-full">
+        <nav className="flex h-20 max-h-20 flex-row w-full justify-between items-center">
+          <img src="/vercel.svg" alt="Logo" width={100} height={"auto"}/>
+          <div>
+            <a href="#aboutProject">Sobre o projeto</a>
+            <button className="rounded-lg border-[2px] border-[#fff] px-2 py-1 text-[#fff] font-semibold">Acessar plataforma</button>
+          </div>
+        </nav>
+      </header>
+    </main>
   );
 }
 
