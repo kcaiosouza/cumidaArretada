c0b07a6b33d685b6c71b407ceac0e771dd10ebec
Caio Souza <kcaiosouza@gmail.com>
2024-05-06 13:37:19 -0300

    alpha-v0.1

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,37 +1,34 @@
 import fastify from "fastify";
 import { z } from "zod"; // Typa e valida variáveis
+import { prismaClient } from "./database/prismaClient";
+import bcrypt from "bcrypt";
 
 const app = fastify();
 const path = "/api/v0.1"
 // REGEX CPF => ([0-9]{3}[\.]?[0-9]{3}[\.]?[0-9]{3}[-]?[0-9]{2})
 // REGEX CNPJ => ([0-9]{2}[\.]?[0-9]{3}[\.]?[0-9]{3}[\/]?[0-9]{4}[-]?[0-9]{2})
 
-app.listen({
-  port: process.env.PORT ? Number(process.env.PORT) : 3333,
-}).then(() => {
-  console.log("HTTP server running!")
-})
-
-/* app.post(path + '/user/login', async (request, reply) => {
-  const { cpf } = z.object({
-		cpf: z.string().regex(new RegExp("([0-9]{3}[\.]?[0-9]{3}[\.]?[0-9]{3}[-]?[0-9]{2})"), "O campo CPF não é válido"),
-	}).parse(request.body)
-
-  return reply.status(200).send()
-})
+app.get("/", (request, reply) => { return reply.send({message: "API Oficial do Cumida Arretada"})})
 
 app.get(path + '/vote/:restaurantId/:userId', async (request, reply) => {
   const { restaurantId, userId } = z.object({
-    restaurantId: z.string(),
-    userId: z.string()
+    restaurantId: z.string().uuid(),
+    userId: z.string().uuid()
   }).parse(request.params)
 
-  reply.send()
+  const vote = await prismaClient.review.findMany({
+    where: {
+      restaurant_id: restaurantId,
+      user_id: userId
+    }
+  })
+
+  reply.status(200).send(vote)
 })
 
 app.post(path + '/vote/:restaurantId', async (request, reply) => {
   const { restaurantId } = z.object({
-    restaurantId: z.string()
+    restaurantId: z.string().uuid()
   }).parse(request.params)
 
   const { userId, rating, waitingTime, service, temperature, ingredient, flavor, presentation, inovation } = z.object({
@@ -46,6 +43,107 @@ app.post(path + '/vote/:restaurantId', async (request, reply) => {
     inovation: z.boolean(),
   }).parse(request.body)
 
+  const vote = await prismaClient.review.create({
+    data: {
+      restaurant_id: restaurantId,
+      user_id: userId,
+      waiting_time: waitingTime,
+      rating,
+      flavor,
+      ingredient,
+      service,
+      inovation,
+      presentation,
+      temperature
+    }
+  })
 
-  reply.status(201).send()
-}) */
+  return reply.status(201).send(vote)
+})
+
+app.post(path + '/user/login', async (request, reply) => {
+  const { cpf, email } = z.object({
+		cpf: z.string().regex(new RegExp("([0-9]{3}[\.]?[0-9]{3}[\.]?[0-9]{3}[-]?[0-9]{2})"), "O campo CPF não é válido"),
+    email: z.string().email(),
+	}).parse(request.body)
+
+  const user = await prismaClient.user.findUnique({
+    where: {
+      cpf,
+      email
+    }
+  })
+
+  var output = {success: false, message: "Não foi encontrado nenhum usuário com as credênciais informadas", user:{} }
+  if(user) {
+    output.message = "Um código foi enviado para o email informado"
+    output.success = true
+    output.user = user
+  }
+
+  return reply.status(200).send(output)
+})
+
+app.post(path + '/user/signup', async (request, reply) => {
+  const { name, cpf, email } = z.object({
+    name: z.string(),
+    cpf: z.string().regex(new RegExp("([0-9]{3}[\.]?[0-9]{3}[\.]?[0-9]{3}[-]?[0-9]{2})"), "O campo CPF não é válido"),
+    email: z.string().email(),
+  }).parse(request.body)
+
+  const userExist = await prismaClient.user.findUnique({
+    where: {
+      cpf
+    }
+  })
+
+  if(!userExist){
+    const user = await prismaClient.user.create({
+      data: {
+        password: "",
+        name,
+        email,
+        cpf
+      }
+    })
+
+    return reply.status(201).send(user)
+  }else{
+    return reply.status(200).send(userExist)
+  }
+})
+
+app.post(path + '/restaurant/register', async (request, reply) => {
+  const { cnpj, name, password } = z.object({
+    cnpj: z.string().regex(new RegExp("([0-9]{2}[\.]?[0-9]{3}[\.]?[0-9]{3}[\/]?[0-9]{4}[-]?[0-9]{2})"), "O campo CNPJ não é válido"),
+    name: z.string(),
+    password: z.string(),
+  }).parse(request.body)
+  
+  const restaurantExist = await prismaClient.restaurant.findUnique({
+    where: {
+      cnpj
+    }
+  })
+
+  if(!restaurantExist) {
+    const hashPassword = await bcrypt.hash(password, 10);
+    const restaurant = await prismaClient.restaurant.create({
+      data: {
+        cnpj,
+        name,
+        password: hashPassword,
+      }
+    })
+
+    return reply.status(201).send(restaurant)
+  }else{
+    return reply.status(409).send(restaurantExist)
+  }
+})
+
+app.listen({
+  port: process.env.PORT ? Number(process.env.PORT) : 3333,
+}).then(() => {
+  console.log("HTTP server running!")
+})
