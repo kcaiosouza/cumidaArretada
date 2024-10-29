a94820cf4bce80191b66fa8dcb03858f5f92a42e
Caio Souza <kcaiosouza@gmail.com>
2024-10-29 08:09:43 -0300

    :bug: fix: URL ngrok

--- web/src/services/api.tsx
+++ web/src/services/api.tsx
@@ -4,7 +4,7 @@ import { parseCookies } from 'nookies';
 const { "CumidaArretada.AuthToken" : authToken } = parseCookies();
 
 export const api = axios.create({
-    baseURL: process.env.API_URL || "https://0bf6-2804-29b8-507a-750f-d55-a521-4644-7b79.ngrok-free.app/api/v0.1"
+    baseURL: process.env.API_URL || "https://93ef-2804-29b8-507a-750f-2e94-dd8f-b268-c2f2.ngrok-free.app/api/v0.1"
 })
 
 if(authToken) {
